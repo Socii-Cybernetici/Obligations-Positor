1be0d09ea665f98f3a1cedc9a00ee4e5c59c73cd
NoraxaXV <kurororbm@gmail.com>
2025-04-08 16:42:36 -0400

    Added nix devenv shell

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Obligations Positor Bot";
+  
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    devenv.url = "github:cachix/devenv";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.devenv.flakeModule
+      ];
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {      
+        devenv.shells.default = pkgs.callPackage ./shell.nix { };
+      };
+    };
+}

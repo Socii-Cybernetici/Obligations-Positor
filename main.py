ae32267f2b7ec6c7da67981e1b16ee1e44119552
NoraxaXV <kurororbm@gmail.com>
2025-04-10 15:18:46 -0400

    added uv pacakge manager and updated devenv

---
+++ main.py
@@ -0,0 +1,8 @@
+import discord
+import z3
+
+def main():
+  print("Goodbye world!")
+
+if __name__ == "__main__":
+    main()
--- shell.nix
+++ shell.nix
@@ -1,9 +1,10 @@
 { pkgs, ... }: {
   # Devenv Docs: https://devenv.sh/basics/
-  languages.python.enable = true;
-  languages.python.package = pkgs.python3.withPackages (python-pkgs: with python-pkgs; [
-    # Add nix-packaged python libraries here: https://search.nixos.org/packages
-    discordpy
-    z3-solver
-  ]);       
+  languages.python = {
+    enable = true;
+    uv = {
+      enable = true;
+      sync.enable = true;
+    };
+  };
 }
